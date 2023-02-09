4553bdb5f707308664d8dfbf0124121f4137489a
PlayerG9 <g9yout@gmail.com>
2023-02-09 15:55:09 +0100

    fixed buggy on mobile menubar and build

--- src/web-ui/src/components/ControlHeader/index.tsx
+++ src/web-ui/src/components/ControlHeader/index.tsx
@@ -3,19 +3,22 @@ import PathBar from "./PathBar";
 
 export default function ControlHeader() {
     const [lastY, setLastY] = useState(window.scrollY);
-    const [isSticky, setSticky] = useState<boolean>(false);
+    const [offset, setOffset] = useState(0);
 
     useEffect(() => {
         const controller = new AbortController();
 
         document.addEventListener("scroll", () => {
             const nowY = window.scrollY;
-            setSticky(nowY < lastY);
+            const diff = nowY - lastY;
+            setOffset(Math.min(Math.max(0, offset + diff/2), 100));
             setLastY(nowY);
         }, { passive: true, signal: controller.signal });
         
         return () => controller.abort();
     });
+
+    const isSticky = offset < 50;
 
     return <div id="control-header" className="top-0 z-50 flex gap-3 px-2 py-px bg-black bg-opacity-75 rounded-md" style={{position: isSticky ? "sticky" : "initial"}}>
         <PathBar />
