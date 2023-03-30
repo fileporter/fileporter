ba63bc7037e48e1727eab0069fe29b8bae6f25ca
PlayerG9 <g9yout@gmail.com>
2023-03-30 10:58:53 +0200

    added audio-support

--- src/web-ui/src/components/SupportedMediaViews/Audio.tsx
+++ src/web-ui/src/components/SupportedMediaViews/Audio.tsx
@@ -1,4 +1,10 @@
-// TODO: implement and register
-export default function Audio() {
-    return null;
+import { apiUrl } from "~/common";
+import { FileTypeResponse } from "~/types";
+
+
+export default function Audio(file: FileTypeResponse) {
+    const srcUrl = apiUrl(`/files/${file.path}`);
+    return <div className="grid h-screen p-1 place-content-center">
+        <audio src={srcUrl} controls className="w-[90vw] rounded-md h-fit" />
+    </div>
 }
--- src/web-ui/src/components/SupportedMediaViews/index.ts
+++ src/web-ui/src/components/SupportedMediaViews/index.ts
@@ -1,11 +1,11 @@
 import { FileTypeResponse } from "~/types";
 import ImageSupport from "./Image";
 import VideoSupport from "./Video";
-// import AudioSupport from "./Audio";
+import AudioSupport from "./Audio";
 
 
 export const MediaSupportIndex: Record<string, ((p: FileTypeResponse) => JSX.Element)> = {
     image: ImageSupport,
     video: VideoSupport,
-    // audio: AudioSupport,
+    audio: AudioSupport,
 }
