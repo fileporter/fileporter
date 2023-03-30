ce6275b26c07639f2d34101ae9ac604612b7e979
PlayerG9 <g9yout@gmail.com>
2023-03-30 11:10:05 +0200

    added autoplay for audio

--- src/web-ui/src/components/SupportedMediaViews/Audio.tsx
+++ src/web-ui/src/components/SupportedMediaViews/Audio.tsx
@@ -5,6 +5,6 @@ import { FileTypeResponse } from "~/types";
 export default function Audio(file: FileTypeResponse) {
     const srcUrl = apiUrl(`/files/${file.path}`);
     return <div className="grid h-screen p-1 place-content-center">
-        <audio src={srcUrl} controls className="w-[90vw] rounded-md h-fit" />
+        <audio src={srcUrl} controls autoPlay className="w-[90vw] rounded-md h-fit" />
     </div>
 }
