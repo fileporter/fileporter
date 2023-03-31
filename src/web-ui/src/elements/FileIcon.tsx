b1ff4e4e806ec09f5133d1dadbf08e2fd6e7eae7
PlayerG9 <g9yout@gmail.com>
2023-03-31 19:44:33 +0200

    reworked icons

--- src/web-ui/src/elements/FileIcon.tsx
+++ src/web-ui/src/elements/FileIcon.tsx
@@ -1,8 +1,11 @@
 import ArchiveIcon from "@assets/files/archive.png";
 import AudioIcon from "@assets/files/audio-file.png";
+// import CloudIcon from "@assets/files/cloud-file.png";
+import CodeIcon from "@assets/files/code-file.png";
+import DocumentIcon from "@assets/files/document.png";
+import HotArticle from "@assets/files/hot-article.png";
 import ImageIcon from "@assets/files/image-file.png";
 import VideoIcon from "@assets/files/video-file.png";
-import DocumentIcon from "@assets/files/document.png";
 import EmptyFileIcon from "@assets/files/default-file.png";
 import { useState } from "react";
 
@@ -13,6 +16,7 @@ interface Props {
     className?: string
 }
 
+
 export default function FileIcon(props: Props) {
     const [imgSrc, setSrc] = useState(props.imgSrc ?? getIconFromMimeType(props.mime));
     const failed = imgSrc !== props.imgSrc;
@@ -22,19 +26,25 @@ export default function FileIcon(props: Props) {
     }} alt="" loading="lazy" />;
 }
 
+
+const MimeMap = {
+    "application/json": CodeIcon,
+    "application/": ArchiveIcon,
+    "audio/": AudioIcon,
+    "image/": ImageIcon,
+    "video/": VideoIcon,
+    "text/x-": CodeIcon,
+    "text/markdown": HotArticle,
+    "text/": DocumentIcon,
+}
+
+
 function getIconFromMimeType(mime: string | undefined): string {
-    switch(mime?.split("/", 1)[0]) {
-        case "application":
-            return ArchiveIcon;
-        case "audio":
-            return AudioIcon;
-        case "image":
-            return ImageIcon;
-        case "video":
-            return VideoIcon;
-        case "text":
-            return DocumentIcon;
-        default:
-            return EmptyFileIcon;
+    if (mime === undefined) return EmptyFileIcon;
+    for (const [pattern, icon] of Object.entries(MimeMap)) {
+        if (mime?.startsWith(pattern)) {
+            return icon;
+        }
     }
+    return EmptyFileIcon;
 }
--- src/web-ui/src/elements/FolderIcon.tsx
+++ src/web-ui/src/elements/FolderIcon.tsx
@@ -1,6 +1,7 @@
-import FolderBackgroundSrc from "@assets/files/folder-background.png";
-import FolderFrontClosedSrc from "@assets/files/folder-front-closed.png";
-import FolderFrontOpenSrc from "@assets/files/folder-front-open.png";
+import FolderIconSrc from "@assets/files/directory.png";
+// import FolderBackgroundSrc from "@assets/files/folder-background.png";
+// import FolderFrontClosedSrc from "@assets/files/folder-front-closed.png";
+// import FolderFrontOpenSrc from "@assets/files/folder-front-open.png";
 
 
 interface Props {
@@ -8,13 +9,24 @@ interface Props {
     className?: string
 }
 
+// not animated (preview on directory pinned)
 export default function FolderIcon(props: Props) {
     return <div className={`relative group ${props.className}`}>
-        <img className="w-full" src={FolderBackgroundSrc} />
+        <img className="w-full" src={FolderIconSrc} />
         {props.previewSrc &&
-            <img className="absolute object-cover w-7/12 border border-black rounded-md top-1 right-2 aspect-square rotate-6" src={props.previewSrc} onError={e => e.currentTarget.style.display = 'none'} />
+            <img className="absolute object-cover w-1/2 border border-black rounded-md top-1/2 left-1/2 -translate-x-[15%] -translate-y-1/3 aspect-square rotate-6" src={props.previewSrc} onError={e => e.currentTarget.style.display = 'none'} />
         }
-        <img className="absolute inset-0 block w-full group-hover:hidden hue-rotate-color" src={FolderFrontClosedSrc} />
-        <img className="absolute inset-0 hidden w-full group-hover:block hue-rotate-color" src={FolderFrontOpenSrc} />
     </div>;
 }
+
+// animated on hovering (preview inside of directory)
+// export default function FolderIcon(props: Props) {
+//     return <div className={`relative group ${props.className}`}>
+//         <img className="w-full" src={FolderBackgroundSrc} />
+//         {props.previewSrc &&
+//             <img className="absolute object-cover w-7/12 border border-black rounded-md top-1 right-2 aspect-square rotate-6" src={props.previewSrc} onError={e => e.currentTarget.style.display = 'none'} />
+//         }
+//         <img className="absolute inset-0 block w-full group-hover:hidden hue-rotate-color" src={FolderFrontClosedSrc} />
+//         <img className="absolute inset-0 hidden w-full group-hover:block hue-rotate-color" src={FolderFrontOpenSrc} />
+//     </div>;
+// }
