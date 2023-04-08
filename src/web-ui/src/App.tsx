b91b4ce86315e0d380049fa35d23f07073d8ca50
PlayerG9 <g9yout@gmail.com>
2023-04-08 18:23:17 +0200

    404 stuff

--- src/web-ui/src/App.tsx
+++ src/web-ui/src/App.tsx
@@ -2,11 +2,12 @@ import { QueryClientProvider } from "react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import HookProviders from "~/hooks/HookProviders";
 import OfflineHeader from "~/components/OfflineHeader";
-import AppPage from "~/pages/app";
-import LoginPage from "./pages/login";
-import SettingsPage from "./pages/settings";
-import URLIndexPage from "./pages/slash";
 import { queryClient } from "./config";
+import AppPage from "~/pages/app";
+import LoginPage from "~/pages/login";
+import SettingsPage from "~/pages/settings";
+import URLIndexPage from "~/pages/slash";
+import Page404NotFound from "~/pages/Page404NotFound";
 
 
 export default function ProviderCollection() {
@@ -30,6 +31,7 @@ export function UIRoutes() {
                 <Route path="login" element={<LoginPage />} />
                 <Route path="settings" element={<SettingsPage />} />
                 <Route path="~/*" element={<AppPage />} />
+                <Route path="*" element={<Page404NotFound />} />
             </Route>
         </Routes>
     </>;
--- src/web-ui/src/pages/app/index.tsx
+++ src/web-ui/src/pages/app/index.tsx
@@ -2,6 +2,7 @@ import ControlHeader from "~/components/ControlHeader";
 import ScrollProgressFix from "~/components/ScrollProgressFix";
 import ViewManager from "~/components/ViewManager";
 
+
 export default function AppPage() {
     return <>
         <ScrollProgressFix />
