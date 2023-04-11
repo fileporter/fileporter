2e472b8d609a7585c7cde2fdbf2d9c331ce1b27c
PlayerG9 <g9yout@gmail.com>
2023-04-11 14:13:37 +0200

    moved old http code

--- src/web-ui/src/config.ts
+++ src/web-ui/src/config.ts
@@ -1,14 +1,26 @@
 import { QueryClient } from "react-query";
-import { HttpError } from "./common";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 
-axios.defaults.baseURL = import.meta.env.PROD ? 
-    import.meta.env.BASE_URL : (() => {
-        const url = new URL(import.meta.env.BASE_URL, window.location.origin);
+export function serverUrl(location: string): string {
+    let url;
+    if (import.meta.env.DEV) {
+        url = new URL(location, window.location.origin)
         url.port = "8000";
-        return url.toString();
-    })();
+    } else {
+        url = new URL(import.meta.env.BASE_URL + location.slice(1), window.location.origin)
+    }
+    return url.toString();
+}
+
+
+axios.defaults.baseURL = serverUrl("/")
+// axios.defaults.baseURL = import.meta.env.PROD ? 
+//     import.meta.env.BASE_URL : (() => {
+//         const url = new URL(import.meta.env.BASE_URL, window.location.origin);
+//         url.port = "8000";
+//         return url.toString();
+//     })();
 axios.defaults.timeout = 15_000;
 axios.defaults.withCredentials = true;
 
@@ -16,8 +28,8 @@ axios.defaults.withCredentials = true;
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            retry: (failureCount, error,) => {
-                if (error instanceof HttpError) {
+            retry: (failureCount, error) => {
+                if (error instanceof AxiosError) {
                     return false;
                 }
                 return failureCount > 3;
