14a79fe399596a1b4584e7f957186de8d712167e
PlayerG9 <g9yout@gmail.com>
2023-04-14 16:19:15 +0200

    improved design for the loading animation

--- src/web-ui/src/elements/Loading.tsx
+++ src/web-ui/src/elements/Loading.tsx
@@ -1,5 +1,9 @@
+import MiniserveIconSrc from "@assets/images/miniserve.png";
+
+
 export default function Loading() {
-    return <div>
-        <div className="w-10 h-10 mx-auto border-t-4 rounded-full border-t-blue-500 animate-spin" />
+    return <div className="h-[90vh] flex">
+        <img className="w-1/2 h-auto max-h-full m-auto animate-pulse" src={MiniserveIconSrc} alt="Loading..." />
+        {/* <div className="w-10 h-10 mx-auto border-t-4 rounded-full border-t-blue-500 animate-spin" /> */}
     </div>;
 }
