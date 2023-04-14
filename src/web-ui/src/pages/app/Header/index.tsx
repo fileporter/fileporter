adac1e0963826f793e437d2b595c8360a4fc5bc1
PlayerG9 <g9yout@gmail.com>
2023-04-14 21:03:16 +0200

    fixed header scroll-overlay

--- src/web-ui/src/pages/app/Header/index.tsx
+++ src/web-ui/src/pages/app/Header/index.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useRef, useState } from "react";
 import PathBar from "./PathBar";
 import ShowSettings from "./ShowSettings";
 import ToggleFullScreen from "./ToggleFullScreen";
 
 
 export default function AppHeader() {
-    return <div className="flex gap-2 bg-black">
+    const [isVisible, setVisible] = useState(true);
+    const lastScrollTop = useRef<number>(0);
+    const isTopMost = window.scrollY <= 0;
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        document.addEventListener("scroll", () => {
+            const scrollTop = window.scrollY;
+            setVisible(scrollTop < lastScrollTop.current);
+            lastScrollTop.current = scrollTop;
+        }, { passive: true, signal: controller.signal });
+
+        return () => controller.abort();
+    });
+
+    return <div className="sticky inset-x-0 z-50 flex gap-3 px-2 py-px transition-[top] duration-300 bg-black bg-opacity-80" style={{top: (isTopMost || isVisible) ? "0px" : "-100%"}}>
         <PathBar />
         <ToggleFullScreen />
         <ShowSettings />
