a0f786f60abcc4be287a4068e68636f7e337bcc6
PlayerG9 <g9yout@gmail.com>
2023-04-14 20:06:50 +0200

    fixed sizing

--- src/web-ui/src/pages/settings/util/OptionSwitch.tsx
+++ src/web-ui/src/pages/settings/util/OptionSwitch.tsx
@@ -15,7 +15,7 @@ interface Props<T> {
 
 export default function OptionSwitch<T>(props: Props<T>) {
     const wf = 1 / props.options.length; // width-factor
-    return <div className="relative flex gap-1 py-px mx-1 overflow-hidden bg-black rounded-xl bg-opacity-70 justify-evenly isolate">
+    return <div className="relative flex w-full max-w-xl gap-1 py-px mx-auto overflow-hidden bg-black rounded-xl bg-opacity-70 justify-evenly isolate">
         <div className="absolute inset-y-0 bg-slate-700 transition-[left] duration-300 -z-10" style={{width: `${wf * 100}%`, left: `${wf * props.options.findIndex(el => el.key === props.current) * 100}%`}} />
         {props.options.map(opt =>
             <button key={opt.key as never} className="w-full" onClick={() => props.onSwitch(opt.key)}>
