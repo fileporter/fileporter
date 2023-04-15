ffeef3ffa7debc89623b53fe2da633ba9329afd5
PlayerG9 <g9yout@gmail.com>
2023-04-15 11:16:40 +0200

    changed loading icon

--- src/web-ui/src/elements/Loading.tsx
+++ src/web-ui/src/elements/Loading.tsx
@@ -1,9 +1,9 @@
-import MiniserveIconSrc from "@assets/miniserve.png";
+// import MiniserveIconSrc from "@assets/miniserve.png";
 
 
 export default function Loading() {
     return <div className="h-[90vh] flex">
-        <img className="w-1/2 h-auto max-h-full m-auto animate-pulse" src={MiniserveIconSrc} alt="Loading..." />
-        {/* <div className="w-10 h-10 mx-auto border-t-4 rounded-full border-t-blue-500 animate-spin" /> */}
+        {/* <img className="w-1/2 h-auto max-h-full m-auto opacity-25 animate-pulse" src={MiniserveIconSrc} alt="Loading..." /> */}
+        <div className="w-10 h-10 mx-auto border-t-4 rounded-full border-t-blue-500 animate-spin" />
     </div>;
 }
