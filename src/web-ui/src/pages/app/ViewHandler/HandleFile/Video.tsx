018a68934a919dc19535ba41abdf57c305034b2f
PlayerG9 <g9yout@gmail.com>
2023-04-15 10:51:05 +0200

    reworked video-handler

--- src/web-ui/src/pages/app/ViewHandler/HandleFile/Video.tsx
+++ src/web-ui/src/pages/app/ViewHandler/HandleFile/Video.tsx
@@ -1,23 +1,13 @@
-import { useState } from "react";
 import type { FileTypeResponse } from "~/types";
 import { serverUrl } from "~/config";
-import ApiFileLink from "~/elements/OpenModeLink/ApiFileLink";
 
 
 export default function VideoSupport(file: FileTypeResponse) {
-    const [videoFailed, setFailed] = useState(false);
     const srcUrl = serverUrl(`/files/${file.path}`);
 
     return <div className="fixed inset-0 w-screen h-screen">
-        {videoFailed ?
-            <div className="absolute text-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                <h1 className="text-xl select-none">Video Failed to load</h1>
-                <ApiFileLink className="text-blue-400 underline" to={file.path}>Open Directly</ApiFileLink>
-            </div>
-            :
-            <video className="object-contain w-full h-full my-auto" controls autoPlay disablePictureInPicture preload="metadata" onError={() => setFailed(true)}>
-                <source src={srcUrl} type={file.mime} />
-            </video>
-        }
+        <video className="object-contain w-full h-full my-auto" controls disablePictureInPicture preload="metadata">
+            <source src={srcUrl} type={file.mime} />
+        </video>
     </div>;
 }
