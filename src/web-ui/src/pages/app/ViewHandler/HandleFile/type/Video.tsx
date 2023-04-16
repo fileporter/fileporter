e3ea31f8c758fafcbd81282267f80f490d7d59b1
PlayerG9 <g9yout@gmail.com>
2023-04-16 21:11:38 +0200

    re-enabled autoplay and new build

--- src/web-ui/src/pages/app/ViewHandler/HandleFile/type/Video.tsx
+++ src/web-ui/src/pages/app/ViewHandler/HandleFile/type/Video.tsx
@@ -6,7 +6,7 @@ export default function VideoSupport(file: FileTypeResponse) {
     const srcUrl = serverUrl(`/files/${file.path}`);
 
     return <div className="fixed inset-0 w-screen h-screen">
-        <video className="object-contain w-full h-full my-auto" controls disablePictureInPicture preload="metadata">
+        <video className="object-contain w-full h-full my-auto" controls autoPlay disablePictureInPicture autoFocus preload="metadata">
             <source src={srcUrl} type={file.mime} />
         </video>
     </div>;
