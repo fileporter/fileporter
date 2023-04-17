65d7f7835981165772ca1790cd1a8e0303edccc9
PlayerG9 <g9yout@gmail.com>
2023-04-17 22:33:22 +0200

    new build

---
+++ src/miniserve/web-ui/assets/Video-3e7104be.js
@@ -0,0 +1,1 @@
+import{s,j as e}from"./index-8609a84a.js";function o(r){const t=s(`/files/${r.path}`);return e.jsx("div",{className:"fixed inset-0 w-screen h-screen",children:e.jsx("video",{className:"object-contain w-full h-full my-auto",controls:!0,autoPlay:!0,disablePictureInPicture:!0,autoFocus:!0,preload:"metadata",children:e.jsx("source",{src:t,type:r.mime})})})}export{o as default};
