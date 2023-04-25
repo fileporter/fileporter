12ceca31df8eca54035549927f846d292cb1dcc1
PlayerG9 <g9yout@gmail.com>
2023-04-25 10:02:16 +0200

    new build

--- src/fileporter/web-ui/assets/Audio-1222b731.js
+++
@@ -1,1 +0,0 @@
-import{s as t,j as r}from"./index-d58286d1.js";function a(e){const s=t(`/files/${e.path}`);return r.jsx("div",{className:"grid h-screen p-1 place-content-center",children:r.jsx("audio",{src:s,controls:!0,autoPlay:!0,className:"w-[90vw] rounded-md h-fit"})})}export{a as default};
