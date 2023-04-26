ad9522b81e2839a8f399178a1282c0fa0a384e99
PlayerG9 <g9yout@gmail.com>
2023-04-26 20:53:04 +0200

    hides logout-button if not logged in

--- src/web-ui/src/pages/settings/LogoutPart.tsx
+++ src/web-ui/src/pages/settings/LogoutPart.tsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
 import LogoutIconSrc from "@assets/icons/logout-icon.png?inline";
+import { useQuery } from "react-query";
+import api from "~/api";
 
 
 export default function LogoutPart() {
+    const query = useQuery(
+        ["is-logged-in"],
+        ({ signal }) => api.isLoggedIn({ signal }),
+    );
+
+    if (!query.isSuccess || !query.data) {
+        return null;
+    }
+
     return <>
         <Link className="w-full max-w-xs px-2 py-1 text-center bg-darker bg-opacity-70 rounded-xl group" to="/logout">
             <img className="inline-block h-4 mr-1 invert" src={LogoutIconSrc} alt="" />
