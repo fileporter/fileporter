56992f442596dd7e54a3f4a4cf8bcdfc41c7f485
PlayerG9 <g9yout@gmail.com>
2023-04-29 17:44:56 +0200

    new build

--- src/fileporter/web-ui/assets/Video-0f16199d.js
+++
@@ -1,1 +0,0 @@
-import{s,j as e}from"./index-553ac612.js";function o(r){const t=s(`/files/${r.path}`);return e.jsx("div",{className:"fixed inset-0 w-screen h-screen",children:e.jsx("video",{className:"object-contain w-full h-full my-auto",controls:!0,autoPlay:!0,disablePictureInPicture:!0,autoFocus:!0,preload:"metadata",children:e.jsx("source",{src:t,type:r.mime})})})}export{o as default};
