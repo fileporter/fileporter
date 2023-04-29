2db87786180c57228aebb0c0aa74b5b43fdf5fba
PlayerG9 <g9yout@gmail.com>
2023-04-29 17:29:02 +0200

    added support for the escape key during contextmenu

--- src/web-ui/src/components/ContextMenu/Provider.tsx
+++ src/web-ui/src/components/ContextMenu/Provider.tsx
@@ -18,8 +18,13 @@ export default function ContextMenuProvider(props: PropsWithChildren) {
         const wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
         const opt = { capture: true, passive: false, signal: controller.signal };
 
-        window.addEventListener(wheelEvent, e => e.preventDefault(), opt); // modern desktop
+        window.addEventListener(wheelEvent, e => e.preventDefault(), opt); // desktop
         window.addEventListener("touchmove", e => e.preventDefault(), opt); // mobile
+        window.addEventListener("keydown", (event) => {
+            if (event.key === "Escape") {
+                setMenu(undefined);
+            }
+        }, opt);
 
         return () => controller.abort();
     }, [menu]);
@@ -27,7 +32,9 @@ export default function ContextMenuProvider(props: PropsWithChildren) {
     return <ContextMenuContext.Provider value={setMenu}>
         {props.children}
         {!!menu && ReactDOM.createPortal(<>
+            {/* background */}
             <div className="fixed inset-0 z-50 bg-black bg-opacity-50 backdrop-blur-sm" onDoubleClick={() => setMenu(undefined)} />
+            {/* actual popup */}
             <div className="fixed z-50 bg-black border rounded-lg bg-opacity-70 centered border-accent">
                 <div className="relative p-2 max-w-[min(500px,90vw)] min-h-[30vh] max-h-[80vh] w-screen">
                     <button className="absolute top-0 right-0 p-1 translate-x-1/2 -translate-y-1/2 bg-black border rounded-xl border-accent" onClick={() => setMenu(undefined)}>
