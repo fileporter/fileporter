79e661dd2c86845a0ee4f6f6dabd82e1caf9684a
PlayerG9 <g9yout@gmail.com>
2023-05-04 21:02:46 +0200

    implemented custom image element

--- src/web-ui/src/elements/Image.tsx
+++ src/web-ui/src/elements/Image.tsx
@@ -1,24 +1,50 @@
 import { createRef, useEffect } from "react";
+import DownloadFailedIcon from "@assets/icons/download-fail.png";
 
 
-export default function Image(props: JSX.IntrinsicElements["img"]) {
+type ImageProps = Omit<JSX.IntrinsicElements["img"], "onError" | "onClick" | "loading">
+
+
+export default function Image(props: ImageProps) {
     const imageRef = createRef<HTMLImageElement>();
 
     // custom lazy loading
     useEffect(() => {
-        if (!imageRef.current) {
+        const image = imageRef.current;
+        if (!image) {
             return;
         }
         const observer = new IntersectionObserver((entries) => {
             entries.forEach(entry => {
+                const img = entry.target as HTMLImageElement;
                 if (entry.isIntersecting) {
-                    (entry.target as HTMLImageElement).src = props.src!;
+                    img.src = props.src!;
+                } else if (!img.complete) {
+                    img.src = ""; // cancel loading in case someone scrolls really fast
                 }
             });
-        }, { rootMargin: "50vh auto" });
-        observer.observe(imageRef.current);
-        return observer.unobserve(imageRef.current);
-    }, []);
+        }, { rootMargin: "100% 0px" });
+        observer.observe(image);
+        return () => observer.unobserve(image);
+    }, [imageRef.current]);
 
-    return <img ref={imageRef} {...props} src={undefined} />;
+    const onError: React.ReactEventHandler<HTMLImageElement> = ({ currentTarget }) => {
+        currentTarget.onerror = null;
+        currentTarget.src = DownloadFailedIcon;
+    };
+
+    return <img ref={imageRef} {...props} src={undefined}
+        onLoad={({ currentTarget }) => {
+            currentTarget.onerror = null;
+        }}
+        onError={onError}
+        onClick={({ currentTarget }) => {
+            if (currentTarget.onerror === null && !currentTarget.complete) {
+                const url = new URL(props.src!);
+                url.searchParams.set("ts", Date.now().toString());
+                currentTarget.src = url.toString();
+                currentTarget.onerror = onError as never;
+            }
+        }}
+    />;
 }
