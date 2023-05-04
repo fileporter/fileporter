fd73396cc9085af589dd8069b9165a54938ab047
PlayerG9 <g9yout@gmail.com>
2023-05-04 16:51:17 +0200

    first concept-version of video player

--- src/web-ui/src/pages/app/ViewHandler/HandleFile/type/Video.tsx
+++ src/web-ui/src/pages/app/ViewHandler/HandleFile/type/Video.tsx
@@ -1,13 +1,14 @@
 import type { FileTypeResponse } from "~/api/types";
 import { serverUrl } from "~/config";
+import VideoPlayer from "~/elements/Video";
 
 
 export default function VideoSupport(file: FileTypeResponse) {
     const srcUrl = serverUrl(`/files/${file.path}`);
 
     return <div className="fixed inset-0 w-screen h-screen">
-        <video className="object-contain w-full h-full my-auto" controls autoPlay disablePictureInPicture autoFocus preload="metadata">
+        <VideoPlayer className="object-contain w-full h-full my-auto">
             <source src={srcUrl} type={file.mime} />
-        </video>
+        </VideoPlayer>
     </div>;
 }
