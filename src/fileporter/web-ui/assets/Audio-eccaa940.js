e2aad444503993505b9d72669439c3736e028e06
PlayerG9 <g9yout@gmail.com>
2023-05-08 10:06:03 +0200

    new build

--- src/fileporter/web-ui/assets/Audio-eccaa940.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:src/fileporter/web-ui/assets/Audio-eccaa940.js
-import{s as t,j as r}from"./index-9d6d10a8.js";function a(e){const s=t(`/files/${e.path}`);return r.jsx("div",{className:"grid h-screen p-1 place-content-center",children:r.jsx("audio",{src:s,controls:!0,autoPlay:!0,className:"w-[90vw] rounded-md h-fit"})})}export{a as default};
-========
-import{s as t,j as r}from"./index-a6515dc5.js";function a(e){const s=t(`/files/${e.path}`);return r.jsx("div",{className:"grid h-screen p-1 place-content-center",children:r.jsx("audio",{src:s,controls:!0,autoPlay:!0,className:"w-[90vw] rounded-md h-fit"})})}export{a as default};
->>>>>>>> 4b37c08b2a17c284592a37373e2b2dd544ca7a5b:src/fileporter/web-ui/assets/Audio-9ce32826.js
--- src/fileporter/web-ui/assets/Video-4d346051.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:src/fileporter/web-ui/assets/Video-4d346051.js
-import{s,j as e}from"./index-9d6d10a8.js";function o(r){const t=s(`/files/${r.path}`);return e.jsx("div",{className:"fixed inset-0 w-screen h-screen",children:e.jsx("video",{className:"object-contain w-full h-full my-auto",controls:!0,autoPlay:!0,disablePictureInPicture:!0,autoFocus:!0,preload:"metadata",children:e.jsx("source",{src:t,type:r.mime})})})}export{o as default};
-========
-import{s,j as e}from"./index-a6515dc5.js";function o(r){const t=s(`/files/${r.path}`);return e.jsx("div",{className:"fixed inset-0 w-screen h-screen",children:e.jsx("video",{className:"object-contain w-full h-full my-auto",controls:!0,autoPlay:!0,disablePictureInPicture:!0,autoFocus:!0,preload:"metadata",children:e.jsx("source",{src:t,type:r.mime})})})}export{o as default};
->>>>>>>> 4b37c08b2a17c284592a37373e2b2dd544ca7a5b:src/fileporter/web-ui/assets/Video-97ad6b15.js
